e1591bf89d37e453ffed74319ca28020e8ee7e2d
oussematurki0 <oussematurki0@gmail.com>
2024-09-13 15:19:33 +0200

    commit

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""FizzBuzz implementation that prints "FizzBuzz" for multiples of 15"""
+""" FizzBuzz
+"""
 import sys
 
 
 def fizzbuzz(n):
-    """Prints numbers from 1 to n with FizzBuzz rules."""
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 15) == 0:
+        if i % 3 == 0 and i % 5 == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
+        elif i % 3 == 0:
             tmp_result.append("Fizz")
-        elif (i % 5) == 0:
+        elif i % 5 == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
